4c8c49b73f75ea4cc9bd835a6b09659eaf889c75
Ishmail C <107496442+Ishmail-C@users.noreply.github.com>
2023-01-28 21:32:18 +0530

    Update entrypoint.sh

--- docker/openssh/alpine_openssh/entrypoint.sh
+++ docker/openssh/alpine_openssh/entrypoint.sh
@@ -12,7 +12,7 @@ echo "<p style='margin-top:0in;margin-right:0in;margin-bottom:8.0pt;margin-left:
 echo "<p style='margin-top:0in;margin-right:0in;margin-bottom:8.0pt;margin-left:0in;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;text-align:center;'><u><span style='font-size:21px;line-height:107%;font-family:"Arial Black",sans-serif;color:#8FAADC;'>Container created Date &amp; Time</span></u></p>" >> /var/www/localhost/htdocs/index.html
 echo "<p style='margin-top:0in;margin-right:0in;margin-bottom:8.0pt;margin-left:0in;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;text-align:center;'><span style='font-size:19px;line-height:107%;font-family:"Arial Black",sans-serif;'>`date`</span></p>" >> /var/www/localhost/htdocs/index.html
 echo "<p style='margin-top:0in;margin-right:0in;margin-bottom:8.0pt;margin-left:0in;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;text-align:center;'><br></p>" >> /var/www/localhost/htdocs/index.html
-i
+
 
 echo "-------------------------WELCOME!!!----------------------"
 echo "This container name               :`hostname`"
